31221d00026470e7828c9c247a31a298f56661ce
Đoàn Minh Phượng <doanminhphuong.208@tcu.edu.vn>
2020-12-31 16:17:42 +0700

    news page

--- wp-content/themes/nhabe360/category.php
+++ wp-content/themes/nhabe360/category.php
@@ -1,13 +1,294 @@
 <?php get_header(); ?>
-<main id="content">
-<header class="header">
-<h1 class="entry-title"><?php single_term_title(); ?></h1>
-<div class="archive-meta"><?php if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } ?></div>
+<main id="is_nhabe360" class="category">
+  <!-- <header class="header">
+<h1 class="entry-title"><?php //single_term_title(); 
+                        ?></h1>
+<div class="archive-meta"><?php //if ( '' != the_archive_description() ) { echo esc_html( the_archive_description() ); } 
+                          ?></div>
 </header>
-<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php get_template_part( 'entry' ); ?>
-<?php endwhile; endif; ?>
-<?php get_template_part( 'nav', 'below' ); ?>
-</main>
-<?php get_sidebar(); ?>
-<?php get_footer(); ?>
+<?php //if ( have_posts() ) : while ( have_posts() ) : the_post(); 
+?>
+<?php //get_template_part( 'entry' ); 
+?>
+<?php //endwhile; endif; 
+?>
+<?php //get_template_part( 'nav', 'below' ); 
+?>
+</main> -->
+  <div class="news wrapper mt-5 pt-2">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-9 col-md-9 col-12">
+          <h1 class="news-title">Tin tức Cen Homes</h1>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house2.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house1.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house3.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house4.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house2.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house1.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house3.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+          <article class="news__item">
+            <div class="row">
+              <div class="col-sm-4">
+                <a href="#" class="news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                  <img class="news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house4.jpg' ?>" alt="">
+                </a>
+              </div>
+              <div class="col-sm-8">
+                <div class="news__content">
+                  <h3 class="news__title">
+                    <a href="#" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                      Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+                    </a>
+                  </h3>
+                  <p class="news__description">
+                    Công ty Cổ phần Cen Quảng Ninh đã chính thức ra mắt vào sáng 25/12 tại khu Hoàng Gia 1 - 19, Vinhomes Dragon Bay, Bến Đoan, Hạ Long, Quảng Ninh. Công ty được hình thành sẽ là bước tiến mới, tăng cường sức mạnh cho hệ thống kinh doanh của Cen Land trên cả nước.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </article>
+
+          <nav class="d-flex justify-content-center" aria-label="Page navigation">
+            <ul class="pagination-section pagination">
+              <li class="page-item disabled">
+                <a class="page-link" href="#" aria-label="Previous">
+                  <span aria-hidden="true">‹</span>
+                </a>
+              </li>
+              <li class="page-item active"><a class="page-link" href="#">1</a></li>
+              <li class="page-item"><a class="page-link" href="#">2</a></li>
+              <li class="page-item"><a class="page-link" href="#">3</a></li>
+              <li class="page-item"><a class="page-link" href="#">4</a></li>
+              <li class="page-item"><a class="page-link" href="#">5</a></li>
+              <li class="page-item disabled"><a class="page-link" href="#">...</a></li>
+              <li class="page-item"><a class="page-link" href="#">12</a></li>
+              <li class="page-item"><a class="page-link" href="#">13</a></li>
+              <li class="page-item"><a class="page-link" href="#">14</a></li>
+              <li class="page-item">
+                <a class="page-link" href="#" aria-label="Next">
+                  <span aria-hidden="true">›</span>
+                  <span class="sr-only">Next</span>
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+        <div class="col-lg-3 col-md-3 col-12">
+          <div class="box-category">
+            <h5 class="box-title">Danh mục tin tức</h5>
+            <ul class="box-category__list">
+              <li class="box-category__item">
+                <a href="#" title="Tin tức Cen Homes">Tin tức Cen Homes</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Kiến trúc – Nhà đẹp">Kiến trúc – Nhà đẹp</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Nội ngoại thất">Nội ngoại thất</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Phong thủy">Phong thủy</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Tài chính – Bất động sản">Tài chính – Bất động sản</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Xây dựng - Quy hoạch">Xây dựng - Quy hoạch</a>
+              </li>
+              <li class="box-category__item">
+                <a href="#" title="Tin tức dự án">Tin tức dự án</a>
+              </li>
+            </ul>
+          </div>
+          <div class="box-feature">
+            <h5 class="box-title box-title--feature">Tin tức mới nhất</h5>
+            <article class="last-news">
+              <a href="#" class="last-news__thumb mr-2" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                <img class="last-news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house4.jpg' ?>" alt="">
+              </a>
+              <a href="#" class="last-news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+              </a>
+            </article>
+            <article class="last-news">
+              <a href="#" class="last-news__thumb mr-2" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                <img class="last-news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house3.jpg' ?>" alt="">
+              </a>
+              <a href="#" class="last-news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+              </a>
+            </article>
+            <article class="last-news">
+              <a href="#" class="last-news__thumb mr-2" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                <img class="last-news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house2.jpg' ?>" alt="">
+              </a>
+              <a href="#" class="last-news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+              </a>
+            </article>
+            <article class="last-news">
+              <a href="#" class="last-news__thumb mr-2" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                <img class="last-news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house1.jpg' ?>" alt="">
+              </a>
+              <a href="#" class="last-news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+              </a>
+            </article>
+            <article class="last-news">
+              <a href="#" class="last-news__link mr-2" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                <img class="last-news__img" src="<?php echo get_template_directory_uri() . '/assets/images/house4.jpg' ?>" alt="">
+              </a>
+              <a href="#" class="last-news__link" title="Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế">
+                Ra mắt Công ty Cổ phần Cen Quảng Ninh: bước chuyển mới nâng tầm vị thế
+              </a>
+            </article>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php //get_sidebar(); 
+  ?>
+  <?php get_footer(); ?>
